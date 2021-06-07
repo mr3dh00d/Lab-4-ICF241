d2225f5aa5a83caf7e212b8f912f7c47886d9d6c
Dereck Pavez <d.pavezarias@uandresbello.edu>
2021-06-07 14:21:19 -0400

    Configuracion para la primera aplicacion

---
+++ app-1/index.js
@@ -0,0 +1,13 @@
+const net = require('net');
+const port = 7001;
+
+var server = net.createServer((socket)=>{
+    setInterval(()=>{
+        var r = (Math.random()*100+1).toFixed(1);
+        socket.write(r.toString());
+    }, 2000);
+})
+
+server.listen(port,()=>{
+    console.log('Sensor 1 activado ('+port+')');
+});
